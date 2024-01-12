0025cc62108edf3f7a1553b9dc0d38d3c86904c6
raju741 <155701790+raju741@users.noreply.github.com>
2024-01-12 10:16:59 +0530

    Update New_class.java
    onemo

--- src/test/java/new_one/New_class.java
+++ src/test/java/new_one/New_class.java
@@ -17,7 +17,7 @@ public class New_class {
 
 		driver.get("https://www.demoblaze.com/");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
 
 	@Test(priority = 1)
